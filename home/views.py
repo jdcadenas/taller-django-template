173f7d1ab9bc5ece44b23fd41bc883fad3d023f6
José Cadenas <jdcadenas@gmail.com>
2024-08-12 13:24:15 -0400

    esto es una modificacion de prueba

--- home/views.py
+++ home/views.py
@@ -2,11 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
-# Create your views here.
-#editar el login del usuario
-###
-##
+# esto esta modificado por seba
 
 
 @login_required(login_url="/accounts/login/")
